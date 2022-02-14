d275a7a07a781b0ecbac630c06e7784291e88e02
lucaliberti <liberti.luca@gmail.com>
2022-02-14 19:04:54 +0100

    finale

--- src/app/observable04/observable04.component.ts
+++ src/app/observable04/observable04.component.ts
@@ -1,17 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';   // importare questo modulo
+import { Component, OnInit } from "@angular/core";
+import { Observable, Subscription } from "rxjs"; // importare questo modulo
 
 @Component({
-  selector: 'app-observable04',
-  templateUrl: './observable04.component.html',
-  styleUrls: ['./observable04.component.css']
+  selector: "app-observable04",
+  templateUrl: "./observable04.component.html",
+  styleUrls: ["./observable04.component.css"],
 })
 export class Observable04Component implements OnInit {
   myObservable: Observable<number>;
 
+  makeSubscription1(): void {
+    this.myObservable.subscribe(
+      (x: number) => { console.log("Observer4: ho ricevuto il dato: " + x); }
+    );
+  }
 
-  makeSubscription(): void {
+  makeSubscription2(): void {
+    this.myObservable.subscribe(
+      (x: number) => { console.log("Observer4: ho ricevuto il dato: " + x); },
+      (e: string) => { console.log("Observer4: ho ricevuto errore: " + e);  }
+    );
+  }
 
+  makeSubscription3(): void {
+    this.myObservable.subscribe(
+      (x: number) => { console.log("Observer4: ho ricevuto il dato: " + x); },
+      (e: string) => { console.log("Observer4: ho ricevuto errore: " + e);  },
+      () => { console.log("Observer4: ho ricevuto complete bye"); }
+    );
+  }
+
+
+
+  constructor() {}
+  ngOnInit() {
     this.myObservable = new Observable<number>(function (observer) {
       let i: number = 0;
       observer.next(i++); // 0
@@ -21,15 +43,5 @@ export class Observable04Component implements OnInit {
       observer.error("numeri finiti");
       observer.next(i++); // questo non viene intercettato
     });
-
-    this.myObservable.subscribe(
-      (x:number) => { console.log('Observer4: ho ricevuto il dato: ' + x )},
-      (e:string) => { console.log('Observer4: ho ricevuto errore: '  + e )},
-      ()         => { console.log('Observer4: ho ricevuto complete bye'  )}
-    );
   }
-
-  constructor() { }
-  ngOnInit() { }
-
 }
